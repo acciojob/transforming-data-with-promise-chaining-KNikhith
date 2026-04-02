e93bad4408723e0bfee46c74b4a5ede619905bed
KNikhith <nikhithkotluri@gmail.com>
2026-04-02 22:34:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ btn.addEventListener("click",()=>{
 
 	new Promise((resolve,reject)=>{
 		setTimeout(()=>{
-			output.textContent=`Result ${input}`;
+			output.textContent=`Result: ${input}`;
 			resolve(input);
 		},2000);
 	})
@@ -15,7 +15,7 @@ btn.addEventListener("click",()=>{
 		return new Promise((resolve,reject)=>{
 			input *=2;
 			setTimeout(()=>{
-				output.textContent=`Result ${input}`;
+				output.textContent=`Result: ${input}`;
 				resolve(input);
 			},2000)
 		})
@@ -33,7 +33,7 @@ btn.addEventListener("click",()=>{
 		return new Promise((resolve,reject)=>{
 			input /=2;
 			setTimeout(()=>{
-				output.textContent=`Result ${input}`;
+				output.textContent=`Result: ${input}`;
 				resolve(input);
 			},1000)
 		})
@@ -42,7 +42,7 @@ btn.addEventListener("click",()=>{
 		return new Promise((resolve,reject)=>{
 			input +=10;
 			setTimeout(()=>{
-				output.textContent=`Final Result ${input}`;
+				output.textContent=`Final Result: ${input}`;
 				resolve(input);
 			},1000)
 		})
