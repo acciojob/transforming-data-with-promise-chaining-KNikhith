a333945f3004e9ef282775f964c2a7ad8f74a52c
KNikhith <nikhithkotluri@gmail.com>
2026-04-02 22:29:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,33 +11,41 @@ btn.addEventListener("click",()=>{
 			resolve(input);
 		},2000);
 	})
-	.then((resolve,reject)=>{
-		input *=2;
-		setTimeout(()=>{
-			output.textContent=`Result ${input}`;
-			resolve(input);
-		},2000);
+	.then((val)=>{
+		return new Promise((resolve,reject)=>{
+			input *=2;
+			setTimeout(()=>{
+				output.textContent=`Result ${input}`;
+				resolve(input);
+			},2000)
+		})
 	})
-	.then((resolve,reject)=>{
-		input -=2;
-		setTimeout(()=>{
-			output.textContent=`Result ${input}`;
-			resolve(input);
-		},1000);
+	.then((val)=>{
+		return new Promise((resolve,reject)=>{
+			input -=2;
+			setTimeout(()=>{
+				output.textContent=`Result ${input}`;
+				resolve(input);
+			},1000)
+		})
 	})
-	.then((resolve,reject)=>{
-		input /=2;
-		setTimeout(()=>{
-			output.textContent=`Result ${input}`;
-			resolve(input);
-		},1000);
+	.then((val)=>{
+		return new Promise((resolve,reject)=>{
+			input /=2;
+			setTimeout(()=>{
+				output.textContent=`Result ${input}`;
+				resolve(input);
+			},1000)
+		})
 	})
-	.then((resolve,reject)=>{
-		input +=10;
-		setTimeout(()=>{
-			output.textContent=`Final Result ${input}`;
-			resolve(input);
-		},1000)
+	.then((val)=>{
+		return new Promise((resolve,reject)=>{
+			input +=10;
+			setTimeout(()=>{
+				output.textContent=`Final Result ${input}`;
+				resolve(input);
+			},1000)
+		})
 	})
 	.catch((err)=>console.log(err));
 })
